006f4c633e9ba74d437532b4f001b3ce53dbbd43
akita11 <akita@ifdl.jp>
2025-10-31 08:10:25 +0900

    with log&graph

--- src/main.cpp
+++ src/main.cpp
@@ -31,8 +31,8 @@ uint32_t t0 = 0;
 bool fLogging = false;
 
 void ShowStatus(){
-	M5.Display.fillRect(200, 0, 120, 20, BLACK);
-	M5.Display.setCursor(200, 0);
+	M5.Display.fillRect(0, 20, 120, 40, BLACK);
+	M5.Display.setCursor(0, 20);
 	if (fLogging == true){
 		M5.Lcd.setTextColor(GREEN);
 		M5.Display.printf("Logging");
@@ -51,19 +51,20 @@ void setup() {
 
   while(!gas.begin())
   {
-    printf("NO Deivces !\n");
+		M5.Display.setCursor(0, 0);
+    M5.Display.printf("No Sensor\n");
     delay(1000);
   }
   printf("The device is connected successfully!\n");
-/*
 	SPI.begin();
-  	while(false == 	SD.begin(GPIO_NUM_4, SPI, 15000000)){
-		M5.Display.setCursor(200, 0);
-		M5.Display.println("no SD");
+  while(false == 	SD.begin(GPIO_NUM_4, SPI, 15000000)){
+		M5.Display.setCursor(0, 0);
+		M5.Display.println("no SD card");
 		delay(500);
 	}
+	M5.Display.clear();
+
 	ShowStatus();
-*/
 
   gas.changeAcquireMode(gas.PASSIVITY);
   delay(1000);
@@ -71,17 +72,10 @@ void setup() {
   gas.setTempCompensation(gas.OFF);
 }
 
-void loop() {
-  printf("Ambient:%s / Cons:%f[%vol] / Temp:%f[degC]\n", gas.queryGasType().c_str(), gas.readGasConcentrationPPM(), gas.readTempC());
-  M5.Display.printf("%.2f[%%vol] %.2f[degC]\n", gas.readGasConcentrationPPM(), gas.readTempC());
-  delay(1000);
-}
-
-/*
 uint16_t px = 0;
 #define X 320
-uint16_t val[X][8]; // 8 values: PM1.0, PM2.5, PM4.0, PM10.0, RH, T, VOC, NOx
-uint16_t color[] = {RED, PURPLE, MAGENTA, ORANGE, CYAN, YELLOW, GREEN, SKYBLUE};
+uint16_t val[X][1]; // 1 value: H2S
+uint16_t color[] = {PURPLE};
 
 uint16_t conv_value(float value, float min, float max) {
 	int16_t v = (value - min) / (max - min) * 240;
@@ -90,66 +84,45 @@ uint16_t conv_value(float value, float min, float max) {
 	return(v);
 }	
 
+#define MAX_conH2S 100 // maximum value: to be fixed
+
 void loop() {
+//  printf("Ambient:%s / Cons:%f[%vol] / Temp:%f[degC]\n", gas.queryGasType().c_str(), gas.readGasConcentrationPPM(), gas.readTempC());
+//  M5.Display.printf("%.2f[%%vol] %.2f[degC]\n", gas.readGasConcentrationPPM(), gas.readTempC());
   uint16_t error;
 
-	float massConcentrationPm1p0, massConcentrationPm2p5, massConcentrationPm4p0, massConcentrationPm10p0, ambientHumidity, ambientTemperature, vocIndex, noxIndex;
+	float conH2S, temp;
 
-	error = sen5x.readMeasuredValues(
-		massConcentrationPm1p0, massConcentrationPm2p5, massConcentrationPm4p0, massConcentrationPm10p0,
-		ambientHumidity, ambientTemperature, vocIndex, noxIndex);
+	conH2S = gas.readGasConcentrationPPM();
+	temp = gas.readTempC();
 
-	// value range:
-	// massConcentration: 0-1000 [ug/m3]
-	// VOX, NOX: 1-500 (10-30sec) [index]
-
-	if (error) printSen55ErrorMessage("Error trying to execute readMeasuredValues():", error);
-	else {
-		printf("%f,%f,%f,%f,", massConcentrationPm1p0, massConcentrationPm2p5, massConcentrationPm4p0, massConcentrationPm10p0);
-	  if (isnan(ambientHumidity)) printf("n/a,"); else printf("%f, ", ambientHumidity);
-	  if (isnan(ambientTemperature)) printf("n/a,"); else printf("%f, ", ambientTemperature);
-	  if (isnan(vocIndex)) printf("n/a,"); else printf("%f, ", vocIndex);
-	  if (isnan(noxIndex)) printf("n/a\n"); else printf("%f\n", noxIndex);
-		val[px][0] = conv_value(massConcentrationPm1p0, 0, 1000);
-		val[px][1] = conv_value(massConcentrationPm2p5, 0, 1000);
-		val[px][2] = conv_value(massConcentrationPm4p0, 0, 1000);
-		val[px][3] = conv_value(massConcentrationPm10p0, 0, 1000);
-		val[px][4] = conv_value(ambientHumidity, 0, 100);
-		val[px][5] = conv_value(ambientTemperature, 0, 100);
-		val[px][6] = conv_value(vocIndex, 1, 500);
-		val[px][6] = conv_value(noxIndex, 1, 500);
-		px = (px + 1) % X;
-		uint16_t x, p;
-		p = px;
-		for (x = 0; x < X; x++){
-			uint8_t t = 0;
-			M5.Display.drawFastVLine(x, 0, 240, BLACK);
-			M5.Display.drawPixel(x, 120, LIGHTGREY);
-			if (x % 4 == 0){
-				M5.Display.drawPixel(x, 60, LIGHTGREY);
-				M5.Display.drawPixel(x, 180, LIGHTGREY);
-			}
-			for (t = 4; t < 8; t++) {
-				if (t < 4 || (t >= 4 && !isnan(val[px][t]))){
-					M5.Display.drawPixel(x, 240 - val[p][t], color[t]);
-				}
-			}
-			p = (p + 1) % X;
+	printf("%.2f,%.2f\n", conH2S, temp);
+	val[px][0] = conv_value(conH2S, 0, MAX_conH2S);
+	px = (px + 1) % X;
+	uint16_t x, p;
+	p = px;
+	for (x = 0; x < X; x++){
+		uint8_t t = 0;
+		M5.Display.drawFastVLine(x, 0, 240, BLACK);
+		M5.Display.drawPixel(x, 120, LIGHTGREY);
+		if (x % 4 == 0){
+			M5.Display.drawPixel(x, 60, LIGHTGREY);
+			M5.Display.drawPixel(x, 180, LIGHTGREY);
 		}
-		M5.Lcd.setCursor(0,0);
-		M5.Lcd.setTextColor(color[0]); M5.Lcd.printf("PM10(0-1000)\n");
-		M5.Lcd.setTextColor(color[1]); M5.Lcd.printf("PM2.5(0-1000)\n");
-		M5.Lcd.setTextColor(color[2]); M5.Lcd.printf("PM40(0-1000)\n");
-		M5.Lcd.setTextColor(color[3]); M5.Lcd.printf("PM10(0-1000)\n");
-		M5.Lcd.setTextColor(color[4]); M5.Lcd.printf("Hum(0-100)\n");
-		M5.Lcd.setTextColor(color[5]); M5.Lcd.printf("Temp(0-100)\n");
-		M5.Lcd.setTextColor(color[6]); M5.Lcd.printf("VOX(1-500)\n");
-		M5.Lcd.setTextColor(color[7]); M5.Lcd.printf("NOx(1-500)\n");
-		ShowStatus();
-		uint32_t tm = millis() - t0;
-		logFile.printf("%d,%f,%f,%f,%f,%f,%f,%f,%f\n", tm, massConcentrationPm1p0, massConcentrationPm2p5, massConcentrationPm4p0,
-			massConcentrationPm10p0, ambientHumidity, ambientTemperature, vocIndex, noxIndex);
+		for (t = 4; t < 8; t++) {
+			if (t < 4 || (t >= 4 && !isnan(val[px][t]))){
+				M5.Display.drawPixel(x, 240 - val[p][t], color[t]);
+			}
+		}
+		p = (p + 1) % X;
 	}
+	M5.Lcd.setCursor(0,0);
+	M5.Lcd.setTextColor(color[0]); M5.Lcd.printf("H2S(0-%d):%.2f/%.1f[degC]\n", MAX_conH2S, conH2S, temp);
+	ShowStatus();
+
+	uint32_t tm = millis() - t0;
+	logFile.printf("%d,%f,%f\n", tm, conH2S, temp);
+
 	for (uint8_t i = 0; i < 100; i++){
 		M5.update();
 		if (M5.BtnA.wasClicked()){
@@ -157,7 +130,7 @@ void loop() {
 				t0 = millis();
 				fLogging = true;
 				logFile = SD.open(LOG_FILENAME, "a");
-				logFile.printf("time[ms],PM10,PM2.5,PM40,PM10,Hum,Temp,VOX,NOx\n");
+				logFile.printf("time[ms],H2S,temp\n");
 				i = 100;
 				ShowStatus();
 			}
@@ -168,7 +141,7 @@ void loop() {
 				ShowStatus();
 			}
 		}
-	    delay(10);
+    delay(10);
 	}
+  delay(1000);
 }
-  */
